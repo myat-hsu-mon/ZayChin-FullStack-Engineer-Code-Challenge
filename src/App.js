704360c4509c5b4711870231ffb9d1c67a484a0e
MyatHsuMon <myathsumon554@gmail.com>
2020-07-02 15:00:24 +0630

    Code challenge for Zaychin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,144 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import ProductCart from './Components/ProductCart/ProductCart';
+import {Container,Row,Col, Navbar, Modal} from 'react-bootstrap';
+import DialogComponent from './Components/Dialog/Dialog';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      title : 'Fruits & Vegetables',
+      data : [], 
+      totalQuantity:0,
+      selectedItemsArray : [],
+      showDialog: false
+    }
+  }
+
+  handleIncreasedOnClick = (itemId) =>{
+    let newArray = [];
+      const updatedData = this.state.data.map(item =>{
+        if(item.id === itemId){
+          item.max_quantity_per_order += 1;   
+          this.setState({
+            totalQuantity : this.state.totalQuantity +1
+          })
+
+          if(this.state.selectedItemsArray.length){
+            newArray = this.state.selectedItemsArray.map((selectedItem,index)=>{
+              if(selectedItem.id === itemId){
+                    selectedItem = Object.assign({...selectedItem},{...item})
+                    return selectedItem;
+              }else if(selectedItem.id !== itemId && index < this.state.selectedItemsArray.length){
+                    return item;
+              }else{
+                return selectedItem;
+              }
+               
+            })
+
+          }else{
+            newArray.push(item);
+          }
+        }
+        return item;
+      })
+
+      this.setState({
+        selectedItemsArray: newArray,
+        data : updatedData
+      })
+
+      // setTimeout(()=>{
+      //   console.log("Dialog plus Array: ", this.state.selectedItemsArray)
+      // },1000)
+  }
+
+  handleDecreasedOnClick = (itemId) =>{
+    let newArray =[];
+        const updatedData = this.state.data.map(item =>{
+          if(item.id === itemId){
+            if(item.max_quantity_per_order >0 ){
+              item.max_quantity_per_order -= 1; 
+
+              this.setState({
+                totalQuantity : this.state.totalQuantity -1
+              })
+
+              newArray =this.state.selectedItemsArray.map(selectedItem =>{
+                if(selectedItem.id === itemId){
+                  selectedItem = Object.assign({...selectedItem},{...item})
+                  return selectedItem;
+                }
+                return selectedItem;  
+              })
+              this.setState({
+                selectedItemsArray: newArray,
+              })
+            }
+          }
+          return item;
+        })
+
+      this.setState({
+        data : updatedData
+      })
+
+  }
+
+
+  handleDialogClick =() =>{
+    
+    this.setState({
+      showDialog: true
+    })
+  }
+
+  componentDidMount(){
+    axios.get('https://api.jsonbin.io/b/5efaf74c0bab551d2b693833')
+    .then((data) => {
+      this.setState({
+        data : [ ...data.data.data]
+      })
+    })
+
+  }
+
+  render(){
+    return (
+      <div className="App" >
+       <Navbar bg="light" sticky="top">
+        {this.state.title} 
+        <div className="cart-div">
+          <button className="cart" onClick={()=>this.handleDialogClick()}>{this.state.totalQuantity}</button>
+          <Modal show={this.state.showDialog}>
+                <DialogComponent selectedItemsArray= {this.state.selectedItemsArray} handleIncreasedOnClick={this.handleIncreasedOnClick} handleDecreasedOnClick={this.handleDecreasedOnClick}/>
+          </Modal>
+        </div>       
+       </Navbar>
+
+        <Container >
+         <Row>           
+          {
+           this.state.data.map((item, index) =>{
+             return(
+              <Col sm={4} key={index} >
+              <ProductCart {...item} id={item.id} handleIncreasedOnClick={this.handleIncreasedOnClick} handleDecreasedOnClick={this.handleDecreasedOnClick} /> 
+              </Col>
+             )
+           })
+          }
+         </Row>
+       </Container>   
+               
+
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/Components/Dialog/Dialog.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Quantity from '../Quantity/Quantity'
+import './Dialog.css';
+function Dialog(props) {
+   console.log("Dialog info:",props.selectedItemsArray)
+   return (
+      props.selectedItemsArray.map((item,index) =>{         
+         return(
+         <div key={index}>
+           <div className="cart-dialogue">
+               <img src={item.thumbnail} alt="fruit"/>
+               {item.name}
+           </div>         
+           <Quantity id={item.id} price={item.price}  quantity={item.max_quantity_per_order} handleDecreasedOnClick={props.handleDecreasedOnClick} handleIncreasedOnClick={props.handleIncreasedOnClick}></Quantity>
+
+         </div>)
+         
+        })
+   )
+}
+
+export default Dialog
